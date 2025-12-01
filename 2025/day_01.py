e4a820dc105c4122fb6133ae99229b0a7f6180ba
Ray Wright <raywright88@gmail.com>
2025-12-01 16:03:57 -0500

    2025 day 1

--- 2025/day_01.py
+++ 2025/day_01.py
@@ -1,2 +1,58 @@
 # Day 1
 
+
+def p1():
+    count = 0
+    point = 50
+    with open("2025/input/day-01.txt") as f:
+        for line in f:
+            line = line.strip()
+            direction = line[0]
+            amount = int(line[1:])
+            if direction == "L":
+                point -= amount
+            else:
+                point += amount
+            if point >= 100:
+                point = point % 100
+            elif point < -99:
+                point = point % -100
+            if point == 0:
+                count += 1
+    print(count)
+
+
+def p2():
+    count = 0
+    points = [50]
+    with open("2025/input/day-01.txt") as f:
+        for line in f:
+            line = line.strip()
+            direction = line[0]
+            amount = int(line[1:])
+            last = points[-1]
+            if direction == "L":
+                this = last - amount
+            else:
+                this = last + amount
+            if this >= 100:
+                count += this // 100
+                this = this % 100
+                if last < 0 and this == 0:
+                    count += 1
+            elif this == 0:
+                count += 1
+            elif this < -99:
+                count += this // -100
+                this = this % -100
+                if last > 0 and this == 0:
+                    count += 1
+            if last * this < 0:
+                count += 1
+            points.append(this)
+    print(count)
+
+
+if __name__ == "__main__":
+    p1()
+    p2()
